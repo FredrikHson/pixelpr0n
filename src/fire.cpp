060008958ea7723f7dad562ae18b7bff0c428917
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2016-06-16 07:31:33 +0000

    BURN!

--- src/fire.cpp
+++ src/fire.cpp
@@ -15,7 +15,7 @@ inline unsigned char lerp(unsigned char x1, unsigned char x2, float p)
 }
 void init_firepal()
 {
-    for(unsigned int i=0; i < 64; i++)
+    for(unsigned int i = 0; i < 64; i++)
     {
         unsigned int offset = (i) * 3;
         FirePal[offset]     = lerp(0, 96, (float)i / 64.0f);
@@ -23,7 +23,7 @@ void init_firepal()
         FirePal[offset + 2] = lerp(0, 96, (float)i / 64.0f);
     }
 
-    for(unsigned int i=0; i < 64; i++)
+    for(unsigned int i = 0; i < 64; i++)
     {
         unsigned int offset = (i + 64) * 3;
         FirePal[offset]     = lerp(96, 255, (float)i / 64.0f);
@@ -31,7 +31,7 @@ void init_firepal()
         FirePal[offset + 2] = lerp(96, 0, (float)i / 64.0f);
     }
 
-    for(unsigned int i=0; i < 64; i++)
+    for(unsigned int i = 0; i < 64; i++)
     {
         unsigned int offset = (i + 128) * 3;
         FirePal[offset]     = lerp(255, 255, (float)i / 64.0f);
@@ -39,7 +39,7 @@ void init_firepal()
         FirePal[offset + 2] = lerp(0, 0, (float)i / 64.0f);
     }
 
-    for(unsigned int i=0; i < 64; i++)
+    for(unsigned int i = 0; i < 64; i++)
     {
         unsigned int offset = (i + 192) * 3;
         FirePal[offset]     = lerp(255, 255, (float)i / 64.0f);
@@ -77,14 +77,55 @@ void drawFire()
 {
     init_heatbuffer();
 
-    for(unsigned int i = 0; i < width * height; i++)
+    heatbuffer[rand() % (heatbuffer_w * heatbuffer_h)] = 255;
+
+    for(int i = 0; i < heatbuffer_w; i++)
+    {
+        unsigned int o = heatbuffer_w * (heatbuffer_h - 1) +i;
+        if(heatbuffer[o]>7)
+        heatbuffer[o] -= 8;
+
+    }
+    for(int i = 0; i < heatbuffer_w/10; i++)
+    {
+        unsigned int o = heatbuffer_w * (heatbuffer_h - 1) + rand() % heatbuffer_w;
+        heatbuffer[o] = (rand() % 55) + 200;
+    }
+
+    for(unsigned int y = 0; y < heatbuffer_h; y++)
+    {
+        heatbuffer[0 + y * heatbuffer_w] = 0;
+        heatbuffer[heatbuffer_w - 1 + y * heatbuffer_w] = 0;
+    }
+
+    for(unsigned int y = 0; y < heatbuffer_h - 1; y++)
     {
 
-        unsigned short color = i%width % 255;
-        color *= 3;
-        pixels[i * 4] = FirePal[color];
-        pixels[i * 4 + 1] = FirePal[color + 1];
-        pixels[i * 4 + 2] = FirePal[color + 2];
+        for(unsigned int x = 1; x < heatbuffer_w - 1; x++)
+        {
+            unsigned int pos = x + y * heatbuffer_w;
+            unsigned short heat = 0;
+            heat += heatbuffer[pos];
+            heat += heatbuffer[pos - 1];
+            heat += heatbuffer[pos + 1];
+            heat += heatbuffer[pos + heatbuffer_w] * 6;
+            heatbuffer[pos] = heat / 9;
+
+        }
+    }
+
+    // copy the heatbuffer to the backbuffer
+    for(unsigned int y = 0; y < height; y++)
+    {
+        for(unsigned int x = 0; x < width; x++)
+        {
+            unsigned short color = heatbuffer[x + 1 + y * heatbuffer_w];
+            color *= 3;
+            unsigned int offset = (x + y * width) * 4;
+            pixels[offset] = FirePal[color];
+            pixels[offset + 1] = FirePal[color + 1];
+            pixels[offset + 2] = FirePal[color + 2];
+        }
     }
 
     printFps();
